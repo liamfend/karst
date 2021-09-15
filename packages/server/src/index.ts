975c640e5f976df04d038977fd2a97029da28f19
Liam Li <398389303@qq.com>
2021-09-15 19:39:58 +0800

    feat(debug): add debugger

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,14 @@ let test = "aaa";
 
 console.log(test);
 
+type Person = {
+  a: string;
+  b?: number;
+};
+const person: Person = {
+  a: "aaa",
+};
+console.log(person);
 const m = test + "a";
 test = "33";
 console.log(m + test);
